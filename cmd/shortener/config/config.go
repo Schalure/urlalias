2bcb4fb3d5ae0c9c184c1307eabcb2b797a5b46b
Aleksandr Shcherbakov <au@schmail.ru>
2023-10-07 23:05:57 +0300

    add check config strings func

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"strconv"
 	"strings"
 )
+
 
 const (
 	AppName = string("github.com/Schalure/urlalias")
@@ -27,30 +29,35 @@ func MustInit(){
 
 	flag.Parse()
 
-
-	args := strings.Split(*Host, ":")
-	if len(args) != 2{
+	if err := checkNetAddres(*Host); err != nil{
 		log.Panicf("Server IP addres and port in not right format: %s. For example: 192.168.1.2:80\n", *Host)
 	}
 
-
-	if(args[0] != "localhost"){
-		if net.ParseIP(args[0]) == nil{
-			log.Panicf("Server IP addres in not right format: %s. For example: 192.168.1.2:port\n", args[0])
-		}
-
-	}
-
-	if _, err := strconv.Atoi(args[1]); err != nil{
-		log.Panicf("Server port in not right format: %s. For example: addresIP:80\n", args[1])
-	}
-
-
-	if(*ResponseBaseAddres != "localhost"){
-		if net.ParseIP(*ResponseBaseAddres) == nil{
-			log.Panicf("Response base addres in not right format: %s. For example: 192.168.1.2\n", *ResponseBaseAddres)
-		}
+	if err := checkNetAddres(*ResponseBaseAddres); err != nil{
+		log.Panicf("Response base addres in not right format: %s. For example: 192.168.1.2:80\n", *ResponseBaseAddres)
 	}
 
 	log.Printf("Server host: %s\t Response base addres: %s\n", *Host, *ResponseBaseAddres)
+}
+
+
+// ------------------------------------------------------------
+//	Check format IP addres and port.
+//	Input:
+//		addres string - for example 127.0.0.1:8080
+//	Output:
+//		err error
+func checkNetAddres(addres string) error{
+	args := strings.Split(addres, ":")
+
+	if(args[0] != "localhost"){
+		if net.ParseIP(args[0]) == nil{
+			return fmt.Errorf("ip addres in not right format: %s. for example: 192.168.1.2:port", args[0])
+		}
+	}
+
+	if _, err := strconv.Atoi(args[1]); err != nil{
+		return fmt.Errorf("ort in not right format: %s. for example: addres:80", args[1])
+	}
+	return nil
 }

7fd2150085df244caa533db2775a0338424f048d
Aleksandr Shcherbakov <au@schmail.ru>
2023-11-24 15:54:16 +0300

    update AliasURLModel

--- internal/app/models/aliasmodel.go
+++ internal/app/models/aliasmodel.go
@@ -2,8 +2,9 @@ package models
 
 // Storage model for long URL and their alias keys
 type AliasURLModel struct {
-	ID       uint64 `json:"uuid"`
-	UserID   uint64 `json:"user_id"`
-	ShortKey string `json:"short_url"`
-	LongURL  string `json:"original_url"`
+	ID          uint64 `json:"uuid" db:"uuid"`
+	UserID      uint64 `json:"user_id" db:"user_id"`
+	ShortKey    string `json:"short_url" db:"short_url"`
+	LongURL     string `json:"original_url" db:"original_url"`
+	DeletedFlag bool   `json:"is_deleted" db:"is_deleted"`
 }

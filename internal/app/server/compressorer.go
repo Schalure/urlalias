3563bdb661e53605141a1449b8b3affd979a0adc
Aleksandr Scherbakov <105049507+Schalure@users.noreply.github.com>
2024-02-28 16:06:04 +0300

    add comments

--- internal/app/server/compressorer.go
+++ internal/app/server/compressorer.go
@@ -6,12 +6,14 @@ import (
 	"github.com/Schalure/urlalias/internal/app/compressor"
 )
 
+//	Compressorer type
 type Compressorer interface {
 	Write(p []byte) (n int, err error)
 	Close() error
 	Type() string
 }
 
+//	Compressorer constructor
 func NewCompressorer(w io.Writer, compressType CompressType) Compressorer {
 
 	switch compressType {

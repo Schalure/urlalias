e5edad4a82bf5d9c48238a02f541393ca521a470
Aleksandr Scherbakov <105049507+Schalure@users.noreply.github.com>
2024-03-07 15:25:18 +0300

    add comments

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,4 +1,25 @@
-// statickint -
+// statickint is used to statically analyze the code of your application
+//
+// How to use:
+// In windows: from command line to execut command from root directory of your solution
+//
+//	.\<path to staticlint.exe> ./...
+//
+// In linux: from terminal to execut command from root directory of your solution
+//
+//	.\<path to staticlint> ./...
+//
+// Description of the analyzers used:
+// 1. Appends analyzer - Package appends defines an Analyzer that detects if there is only one variable in append.
+// 2. Assign analyzer - Package assign defines an Analyzer that detects useless assignments.
+// 3. Bools analyzer - Package bools defines an Analyzer that detects common mistakes involving boolean operators.
+// 4. Buildtag analyzer - Package buildtag defines an Analyzer that checks build tags.
+// 5. Copylock analyzer - Package copylock defines an Analyzer that checks for locks erroneously passed by value.
+// 6. Defers analyzer - Package defers defines an Analyzer that checks for common mistakes in defer statements.
+// 7. Fieldalignment analyzer - Package fieldalignment defines an Analyzer that detects structs that would use less memory if their fields were sorted.
+// 8. Nilfunc analyzer - Package nilfunc defines an Analyzer that checks for useless comparisons against nil.
+//
+// For more inforation about analyzers see: https://pkg.go.dev/golang.org/x/tools/go/analysis#section-sourcefiles
 package main
 
 import (
@@ -8,6 +29,14 @@ import (
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/appends"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/buildtag"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 	"golang.org/x/tools/go/analysis/passes/printf"
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/shift"
@@ -20,6 +49,14 @@ func main() {
 
 	checks := []*analysis.Analyzer{
 		//	standart analyzers
+		appends.Analyzer,
+		assign.Analyzer,
+		bools.Analyzer,
+		buildtag.Analyzer,
+		copylock.Analyzer,
+		defers.Analyzer,
+		fieldalignment.Analyzer,
+		nilfunc.Analyzer,
 		printf.Analyzer,
 		shadow.Analyzer,
 		shift.Analyzer,
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/jackc/pgx/v5 v5.5.4
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
-	golang.org/x/tools v0.12.1-0.20230825192346-2191a27a6dc5
+	golang.org/x/tools v0.19.0
 	honnef.co/go/tools v0.4.7
 )
 
@@ -23,9 +23,8 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
-	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
